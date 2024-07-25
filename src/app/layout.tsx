24c6dc24d3cd81ec70fe1c4221b0ecc0fc9e5687
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2024-07-24 23:21:20 -0400

    finish setup basic configs

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Lato } from "next/font/google";
+import "../styles/globals.css";
+
+
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["300", "400", "700", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Shine&Polish",
+  description: "Cleaning service Atlanta",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={lato.className}>
+            <main className="lg:ml-[200px] xl:ml-[244px]">{children}</main>
+      </body>
+    </html>
+  );
+}
