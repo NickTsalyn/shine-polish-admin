b3888537c4635dfd830769b4af71a5f1b51fa6a5
YuliaSilk <yuliashevchyk0506@gmail.com>
2024-07-30 23:37:44 +0300

    start creating scheduler component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,65 @@
-'use client';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+"use client";
+import ScheduleComponent from "@/components/Scheduler";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {data} from "./data/date-bookings";
 
 const getData = async () => {
-	try {
-	  const response = await axios.get("https://shine-polish-server.onrender.com/bookings/options");
-	  return response.data;
-	} catch (error) {
-	  console.error(error);
-	}
-  }
-
-
-
+ try {
+  const response = await axios.get("https://shine-polish-server.onrender.com/bookings/options");
+  return response.data;
+ } catch (error) {
+  console.error(error);
+ }
+};
 
 export default function Home() {
-	const [result, setResult] = useState([]);
+ const [result, setResult] = useState([]);
 
-	useEffect(() => {
-		const fetchData = async () => {
-            try {
-                const response = await axios.get("https://shine-polish-server.onrender.com/bookings/options");
-				
-                setResult(response.data);
-				// const areas = response.data.areaOptions;
-				return response.data;
-				// console.log(areas)
-            } catch (error) {
-                console.error(error);
-            }
-        };
-		
-		fetchData();
-		// console.log(data)
-	  }, []);
+ useEffect(() => {
+  const fetchData = async () => {
+   try {
+    const response = await axios.get("https://shine-polish-server.onrender.com/bookings/options");
 
-	//   useEffect(() => {
-	// 	console.log(data);
-	//   }, [data]);
+    setResult(response.data);
+    // const areas = response.data.areaOptions;
+    return response.data;
+    // console.log(areas)
+   } catch (error) {
+    console.error(error);
+   }
+  };
 
-	  console.log(result)
+  fetchData();
+  // console.log(data)
+ }, []);
 
-	  
-	return (
-		<div>
-			<h1>Hello Admin page</h1>
-			<form >
-				<input type="text" className=" border border-s-orange-500"/>
-				<input type="text" className=" border border-s-orange-500"/>
-				<button type="submit">Submit</button>
-			</form>
-			<ul>
-				
-			</ul>
-		</div>
-	);
+ //   useEffect(() => {
+ // 	console.log(data);
+ //   }, [data]);
+
+ console.log(result);
+
+ return (
+  <div>
+   <h1>Hello Admin page</h1>
+   <form>
+    <input
+     type="text"
+     className=" border border-s-orange-500"
+    />
+    <input
+     type="text"
+     className=" border border-s-orange-500"
+    />
+    <button type="submit">Submit</button>
+   </form>
+   <ScheduleComponent
+   // eventSettigs={{
+   //  dataSourse: data,
+   // }}
+   />
+   <ul></ul>
+  </div>
+ );
 }
