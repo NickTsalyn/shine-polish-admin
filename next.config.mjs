47baaf18fca65da94550caefcfbc0f2241c60813
MaksymGS <130307586+MaksymGS@users.noreply.github.com>
2024-10-19 17:12:51 +0300

    uncomment nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // images: {
-    //     domains: ['res.cloudinary.com'],
-    //   },
-    //   compiler: {
-    //     removeConsole: true,
-    //   },
+    images: {
+        domains: ['res.cloudinary.com'],
+      },
+      compiler: {
+        removeConsole: true,
+      },
 };
 
 
