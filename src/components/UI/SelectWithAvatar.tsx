84cab946026024521d5473391518210637ee2d15
MaksymGS <130307586+MaksymGS@users.noreply.github.com>
2024-10-21 19:35:16 +0300

    fix selectWithAvatar

--- src/components/UI/SelectWithAvatar.tsx
+++ src/components/UI/SelectWithAvatar.tsx
@@ -6,21 +6,22 @@ import MenuItem from "@mui/material/MenuItem";
 import { GetEmployee } from "@/types/interfaces";
 
 function renderValue(option: any | null) {
-  console.log(option);
-  console.log(option.avatar);
   if (!option) {
     return null;
   }
-
+  let selectedItem = option;
+  if (typeof option === "string") {
+    selectedItem = JSON.parse(option);
+  }
   return (
     <React.Fragment>
       <div className="flex gap-5 items-center">
         <Avatar
-          src={option.avatar}
+          src={selectedItem.avatar}
           sx={{ width: 70, height: 70 }}
           className="absolute -top-2 -left-2 z-10"
         />
-        <p className="ml-14">{option.username}</p>
+        <p className="ml-14">{selectedItem.username}</p>
       </div>
     </React.Fragment>
   );
@@ -31,8 +32,6 @@ interface SelectWithAvatarProps {
 }
 
 export default function SelectWithAvatar({ employees }: SelectWithAvatarProps) {
-  console.log(employees);
-
   const [value, setValue] = React.useState(employees[0]);
 
   const handleChange = (event: SelectChangeEvent<typeof value>) => {
@@ -47,7 +46,11 @@ export default function SelectWithAvatar({ employees }: SelectWithAvatarProps) {
       onChange={handleChange}
     >
       {employees.map((employee: GetEmployee, index: number) => [
-        <MenuItem key={index} value={employee} className="gap-6 p-2 m-0">
+        <MenuItem
+          key={index}
+          value={JSON.stringify(employee)}
+          className="gap-6 p-2 m-0"
+        >
           <Avatar src={employee.avatar} />
           {employee.username}
         </MenuItem>,
